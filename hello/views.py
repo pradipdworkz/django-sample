daf849e6a5775e85687be587aecb2b1d572ced33
pradip <pradipd@workz.com>
2021-09-29 11:29:30 +0400

    Fix return issue

--- hello/views.py
+++ hello/views.py
@@ -18,4 +18,4 @@ def hello_world(request, format=None):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
